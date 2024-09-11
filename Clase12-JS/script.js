f375494840e6509543c6fa07c1a4845215d9c01a
Aguss <agustinampo80@gmail.com>
2024-09-11 16:36:50 -0300

    JavaScript 1°

---
+++ Clase12-JS/script.js
@@ -0,0 +1,62 @@
+/* TIPOS DE DATOS */
+
+/* OBJETOS LO VEMOS EN OTRO CAPITULO */
+/* PRIMITIVOS */
+
+/*
+STRINGS: Se escriben entre comillas ('', "",´´) y representan palabras alfa numericas o textos
+BOOLEANOS BOOLEAN: true or false se usan para representar estados booleanos o binarios
+NUMBERS: 7, 6.2(FLOAT), -5
+NULOS: Representa ausencia de dato null
+UNDERFINED: underfined Representa un dato sin definir
+*/
+// "Pepe-1234"
+// 'Hola123'
+// Booleanos
+true
+false
+// Numeros
+2
+2.2
+-2
+NaN // Not A Number, aparece cuando intento transformar a un dato a numero, q no se puede transformar a numero Dato Numerico
+
+Infinity // Se suele usar cuando se divide algo por cero dato Numerico
+
+// Constructor Number
+
+// Number
+// alert(Number("Pepe"))
+
+// Constructores
+// Nos permiten pasar de un dato a otro dato
+
+Number
+Boolean
+String
+
+// typeof le pasamos el tipo de dato y nos devuelve como string el tipo de dato
+console.log( 
+    typeof(
+        Number(true) //'number'
+    ) 
+)
+
+console.log( 
+    typeof(
+        Boolean(1) //'boolean'
+    ) 
+)
+
+// Consola: es una herramienta que nos permite mostrar valores de nuestro programa
+
+// console.log = registrar en consola
+console.log("Hola")
+
+/* 
+Operadoresd Aritmeticos
+Comparadores
+Operadores logicos
+Variables
+Condiciones
+*/
