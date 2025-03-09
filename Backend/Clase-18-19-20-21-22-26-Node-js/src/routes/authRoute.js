c439815c3f63a9367b6cc9ec200a949fc0d78378
Aguss <agustinampo80@gmail.com>
2025-03-09 09:31:37 -0300

    Conectar backen con front

--- Backend/Clase-18-19-20-21-22-26-Node-js/src/routes/authRoute.js
+++ Backend/Clase-18-19-20-21-22-26-Node-js/src/routes/authRoute.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import registerUsers, { loginController, resetPasswordController, rewritePasswordController, verifyEmailController } from '../controllers/auth.controller.js'
+import { createWorkspaceController } from '../controllers/workspace.controller.js'
+import { createChannelController } from '../controllers/channel.controller.js'
 
 const authRoute = express.Router()
 
@@ -7,6 +9,7 @@ authRoute.post('/register', registerUsers)
 authRoute.get('/verify-email', verifyEmailController)
 authRoute.post('/login', loginController)
 authRoute.post('/reset-password', resetPasswordController)
-authRoute.put("/rewrite-password", rewritePasswordController)
+authRoute.put('/rewrite-password', rewritePasswordController)
+authRoute.post('/workspaces', createWorkspaceController, createChannelController)
 
 export default authRoute
--- Backend/FRONTEND/src/Screens/CreateWorkspaces.jsx
+++ Backend/FRONTEND/src/Screens/CreateWorkspaces.jsx
@@ -1,30 +1,52 @@
 import React from "react"
 import './global.css'
 import { useNavigate } from "react-router-dom"
+import { useForm } from "../hooks/useForm"
+import { useApiRequest } from "../hooks/useApiRequest"
+import { ENVIROMENT } from "../config/enviroment"
 
 const CreateWorkspaceScreen = () => {
     const navigate = useNavigate()
-
-    const handleClick = () => {
-        navigate('/channel')
+    const initialFormState = {
+        workspaces: '',
+        channel: ''
     }
+    
+    const {formState, handleChangeInput} = useForm(initialFormState)
+    const {responseApiState, postRequest} = useApiRequest(ENVIROMENT.URL_API + '/api/workspaces')
+    
+    const handleSumbitForm = async (event) => {
+        event.preventDefault()
+        if (formState.workspaces && formState.channel) {
+            await postRequest(formState)
+            // navigate('/channel')
+        } else {
+            alert("Por favor, ingresa ambos campos.")
+        }
+    }
+
 
     return (
         <div className="father">
-            <div className="box-workspaces">
+            <form onSubmit={handleSumbitForm} className="box-workspaces">
                 <h1 className="create-h1">Create Workspaces</h1>
                 <div>
                         <label htmlFor="workspaces" className="label-workspaces">Name Workspaces : </label>
-                        <input type="text" id="workspaces" name="workspaces"/>
+                        <input type="text" id="workspaces" name="workspaces" placeholder="UTN-TN-MAR-JUEV" value={formState.workspaces} onChange={handleChangeInput}/>
                 </div>
                 <div>
                         <label htmlFor="channel" className="label-workspaces">Name Channel : </label>
-                        <input type="text" id="channel" name="channel"/>
+                        <input type="text" id="channel" name="channel" placeholder="Tarea alumnos" value={formState.channel} onChange={handleChangeInput}/>
                 </div>
-                <div className="btn-box">
-                    <button type="sumbit" onClick={handleClick}>Enter Workspaces</button>
-                </div>
-            </div>
+                {responseApiState.error && <span style={{color: 'red'}}>{responseApiState.error}</span>}
+                {
+                    responseApiState.loading
+                        ? <span>Se creo con Exito</span>
+                        : <div className="btn-box">
+                            <button type="sumbit">Enter Workspaces</button>
+                        </div>
+                }
+            </form>
         </div>
     )
 }
